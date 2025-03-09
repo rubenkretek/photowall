46343312ae97ac17a21dd8781e34540b769d62b8
rubenkretek <rubenkretek@outlook.com>
2025-03-09 20:18:00 +0000

    mvp

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,82 @@
 import Image from "next/image";
+import { client } from "@/sanity/lib/client";
+import { PHOTOS_QUERY } from "@/sanity/lib/queries";
+import { urlFor } from "@/sanity/lib/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+export default async function Home() {
+  const photos = await client.fetch(PHOTOS_QUERY);
+  console.log(photos.length);
+  const smallThumbnails = Array.from(
+    { length: 100 - photos.length },
+    (_, index) => <SmallThumbnail key={index} opacityValue={index + 1} />
   );
+
+  const thumbnails = Array.from({ length: 100 - photos.length }, (_, index) => (
+    <Thumbnail key={index} opacityValue={index + 1} />
+  ));
+
+  return (
+    <main>
+      <div className="container mx-auto px-6 py-20">
+        <h1 className="text-4xl font-bold text-center sm:text-5xl">
+          Photos I need to take until I can upgrade my gear
+        </h1>
+        <section className="py-10">
+          <div className="flex">
+            {photos.map((photo) => (
+              <div key={photo._id} className="flex">
+                {photo.mainImage ? (
+                  <Image
+                    src={urlFor(photo.mainImage)
+                      .width(80)
+                      .height(80)
+                      .quality(100)
+                      .auto("format")
+                      .url()}
+                    alt={photo?.mainImage?.alt || ""}
+                    width="40"
+                    height="40"
+                    style={{ objectFit: "cover", height: "100%" }}
+                  />
+                ) : null}
+              </div>
+            ))}
+            {smallThumbnails}
+          </div>
+        </section>
+        <section className="py-10">
+          <div className="flex flex-wrap">
+            {thumbnails}
+            {photos.map((photo) => (
+              <div key={photo._id} className="w-40 h-40">
+                {photo.mainImage ? (
+                  <Image
+                    src={urlFor(photo.mainImage)
+                      .width(280)
+                      .height(280)
+                      .quality(100)
+                      .auto("format")
+                      .url()}
+                    alt={photo?.mainImage?.alt || ""}
+                    width="160"
+                    height="160"
+                  />
+                ) : null}
+              </div>
+            ))}
+          </div>
+        </section>
+      </div>
+    </main>
+  );
+}
+
+function SmallThumbnail({ opacityValue }) {
+  const style = { opacity: opacityValue / 100 };
+  return <div className="h-10 w-10 bg-white" style={style}></div>;
+}
+
+function Thumbnail({ opacityValue }) {
+  const style = { opacity: opacityValue / 100 };
+  return <div className="h-40 w-40 bg-white" style={style}></div>;
 }
---
+++ sanity/lib/client.ts
@@ -0,0 +1,11 @@
+// ./src/sanity/lib/client.ts
+import { createClient } from "next-sanity";
+
+import { apiVersion, dataset, projectId } from "../env";
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion, // https://www.sanity.io/docs/api-versioning
+  useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
+});
